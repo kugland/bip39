83f1bcb3a95685ad5da99b177177dc978f216c77
André Kugland <kugland@gmail.com>
2021-10-25 15:53:11 -0300

    ignore output of system()

--- bip39.c
+++ bip39.c
@@ -33,7 +33,7 @@ char *to_binary(const void *data, size_t no_of_bits)
 }
 
 void help(void) {
-  (void) system("man 1 bip39");
+  int _ = system("man 1 bip39");
 }
 
 int main(int argc, char *argv[])
